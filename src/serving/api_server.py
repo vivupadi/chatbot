91bb05884d73f929a2482664e90325ba3bd4f39b
vivupadi <vivupadi@gmail.com>
2025-10-28 23:51:59 +0100

    html part started

--- src/serving/api_server.py
+++ src/serving/api_server.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
 from pydantic import BaseModel, Field
 from typing import List, Optional, Dict
 import logging
@@ -14,23 +15,6 @@ from src.utils.config import settings
 #set logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-
-#Initilize FatAPI
-app = FastAPI(
-    title = 'RAG chatbot API',
-    description= 'API for chatbot backend',
-    version = '0.1'
-)
-
-# CORS middleware
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 # Global instances (initialized on startup)
 rag_engine: Optional[RAGengine] = None
@@ -60,8 +44,8 @@ class IngestResponse(BaseModel):
 
 
 #Startup event
-@app.on_event("startup")
-async def startup_event():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     global rag_engine, vector_store, document_ingestion   #Intialize everything on startup
 
     logger.info("=====Starting RAG Chatbot API=====")
@@ -73,7 +57,30 @@ async def startup_event():
     except Exception as e:
         logger.error(f"Failed to initialize services:{e}")
         raise
+
+    #App runs here
+    yield
+
+
+    ##Shutdown
+    logger.info("Shutting down RAG Chatbot")
     
+#Initilize FatAPI
+app = FastAPI(
+    title = 'RAG chatbot API',
+    description= 'API for chatbot backend',
+    version = '0.1',
+    lifespan=lifespan
+)
+
+# CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 #Query endpoint
 @app.post("/query", response_model = QueryResponse)
@@ -89,3 +96,19 @@ async def query_endpoint(request: QueryRequest):
         logger.error(f"Query failed:{e}")
         raise HTTPException(status_code=500, detail= f"Query failed{e}")
     
+
+# Root endpoint
+@app.get("/")
+async def root():
+    """Root endpoint"""
+    return {
+        "message": "RAG MLOps Chatbot API",
+        "version": "0.1.0",
+        "endpoints": {
+            "query": "/query",
+        }
+    }
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
