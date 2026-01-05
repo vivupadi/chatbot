91db723a531c9925bd494b86d29b31d9f59d8688
vivupadi <vivupadi@gmail.com>
2026-01-05 22:20:14 +0100

    vectorstore asserted

--- tests/vectorstore_tests.py
+++ tests/vectorstore_tests.py
@@ -1,16 +1,20 @@
 import pytest
 import json
 #from src.utils.config import settings
+import sys
+from pathlib import Path
+# Add parent directory to Python path
+sys.path.insert(0, str(Path(__file__).parent.parent))
 from src.data.vectorstore import VectorStore
 
 #Check data presence
-def check_processed_and_vectorised_data_existence():
+def test_check_processed_and_vectorised_data_existence():
     # Load documents
     with open("./data/processed/documents.json", 'r', encoding='utf-8') as f:
         documents = json.load(f)
     assert documents is not None
 
-def check_vectorstore_is_initialized():
+def test_check_vectorstore_is_initialized():
     #from src.data.vectorstore import VectorStore
     vs = VectorStore()
     assert vs.embeddings is not None  #check for vector embedding model 
